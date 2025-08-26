c2d6f695271c2542f8b68c0e8925bc97650c4f54
Aung <potcharaphol.c@gmail.com>
2025-08-27 01:08:13 +0700

    Update Vite configuration base path for local development

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rov-profile/', // Replace with your repository name
+  base: './', // Replace with your repository name
   build: {
     outDir: 'dist',
   },
